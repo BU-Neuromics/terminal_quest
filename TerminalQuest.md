37e89a30646481b5f47948c9830a4213583561df
Adam Labadorf <alabadorf@gmail.com>
2017-01-18 18:23:24 -0500

    up to level 5 implemented

--- TerminalQuest.md
+++ TerminalQuest.md
@@ -25,4 +25,19 @@ structure. The levels are as follows:
   4. all of the other directories have a fake level3 file in them, and they all
      are actually hard links to the same directory that leads nowhere
 
-## Level 3 - 
+## Level 3 - ls and globbing
+
+  1. there are a lot of files in this directory, most with random names
+  2. there are also a lot of hidden directories with random names
+  3. by doing an ls *SECRET files get printed out that reveal the name of the
+     correct hidden directory to follow
+
+## Level 4 - looking at files to find the file path to the right answer
+
+  1. there are 30 files in this directory, each that contains another filename
+     or a hidden directory name
+  2. there is a chain of 5 files that lead to the file with the directory name
+     of the next level, all the other files contain random filenames that do not
+     exist
+
+## Level 5 - 
--- terminal_quest.py
+++ terminal_quest.py
@@ -1,59 +1,127 @@
-from collections import namedtuple
+from collections import defaultdict
 from glob import glob
+import random
 import os
 import shutil
-
-Level = namedtuple('Level',['name','desc','size'])
-level_texts = {}
-
-level_mds = glob('*.md')
-
-for level_md in level_mds :
-  f = open(level_md)
-  desc = f.read()
-  size = f.tell()
-  level_texts[level_md] = Level(
-    level_md
-    ,desc
-    ,size
-  )
-
-print level_texts
-
-level_dirs = [
-  'level0'
-  ,'level1'
-  ,'.level2'
-  ,str(level_texts['level2.md'].size)
-]
-level_paths = [os.path.join(*level_dirs[:_]) for _ in range(1,len(level_dirs)+1)]
+import string
 
 # clean
 try :
-  shutil.rmtree(level_dirs[0])
+  shutil.rmtree('level0')
 except Exception,e :
   print e
 
+def copy_md(level,path) :
+
+    level_md = '{}.md'.format(level)
+
+    if not os.path.exists(level_md) :
+      open(level_md,'w')
+
+    f = open(level_md)
+    desc = f.read()
+    size = f.tell()
+    open(os.path.join(path,level_md),'w').write(desc)
+
+    return level_md,desc,size
+
+def create_mds(level,path) :
+    copy_md(level,path)
+    copy_md('.'+level+'_hint',path)
+
+opj = os.path.join
+
+def rndchr(n) :
+    return ''.join(random.sample(string.ascii_letters,n))
+
 if __name__ == '__main__' :
 
-  path_iter = iter(level_paths)
-  
-  # initial
-  p = next(path_iter)
-  os.mkdir(p)
+    ##################################################
+    # level 0
+    lpath = 'level0'
+    os.mkdir(lpath)
+    create_mds('level0',lpath)
+    ##################################################
 
-  # level 0
-  p = next(path_iter)
-  os.mkdir(p)
+    ##################################################
+    # level 1
+    lpath = opj(lpath,'level1')
+    os.mkdir(lpath)
+    create_mds('level1',lpath)
+    ##################################################
 
-  # level 1
-  p = next(path_iter)
-  os.mkdir(p)
+    ##################################################
+    # level 2
+    lpath = opj(lpath,'.level2')
+    os.mkdir(lpath)
 
-  # level 2
-  p = next(path_iter)
-  os.mkdir(p)
+    level2_md, level2_desc, level2_size = copy_md('level2',lpath)
+    copy_md('.level2_hint',lpath)
 
-  # level 3
-  p = next(path_iter)
-  os.mkdir(p)
+    # create 49 directories named as random numbers between 0-300
+    paths = random.sample(range(0,300),49)
+    while level2_size in paths :
+      paths.remove(level2_size)
+      paths.append(random.randint(0,300))
+    paths = [str(_) for _ in paths]
+    for path in paths:
+      os.mkdir(os.path.join(lpath,path))
+      # put a decoy md file in each path
+      open(opj(lpath,path,'level3.md'),'w').write("# this is not the level you're looking for")
+      open(opj(lpath,path,'.level3_hint.md'),'w').write("# this is not the level you're looking for")
+    ##################################################
+
+    ##################################################
+    # level 3
+    lpath = opj(lpath,str(level2_size))
+    os.mkdir(lpath)
+    create_mds('level3',lpath)
+
+    # create a bunch of random dot directories
+    for _ in range(100) :
+      rnd_dir = '.'+rndchr(5)
+      os.mkdir(opj(lpath,rnd_dir))
+      open(opj(lpath,rnd_dir,'level4.md'),'w').write(
+        "# other directories might have what you seek, but not this one"
+      )
+      open(opj(lpath,rnd_dir,'.level4_hint.md'),'w').write(
+        "# other directories might have what you seek, but not this one"
+      )
+
+    # use the last random directory as the real one
+    real_fns = [rndchr(6).upper()+'_{}_SECRET' for _ in range(len(rnd_dir))]
+    real_fns.sort()
+    real_fns = [_1.format(_2) for _1, _2 in zip(real_fns,rnd_dir)]
+
+    # create a bunch of files with the pattern [A-Z]{6}_[a-z]_[A-Z]{6}
+    fake_fns = [rndchr(6).upper()+'_x_'+rndchr(6).upper() for _ in range(1000)]
+    for fn in real_fns+fake_fns :
+      open(opj(lpath,fn),'w')
+    ##################################################
+
+    ##################################################
+    # level 4
+    lpath = opj(lpath,rnd_dir)
+    # this directory was already created in the previous step
+    create_mds('level4',lpath)
+
+    level5_dir = '.'+rndchr(6)
+    true_file_path = [rndchr(6)+'.txt' for _ in range(5)]
+
+    # create the file chain
+    open(opj(lpath,'start.txt'),'w').write(true_file_path[0])
+    for src,tgt in zip(true_file_path[:-2],true_file_path[1:-1]) :
+        open(opj(lpath,src),'w').write(tgt)
+    open(opj(lpath,true_file_path[-1]),'w').write(level5_dir)
+
+    # create a bunch of other files that contain random filenames
+    for _ in range(25) :
+        fn = rndchr(6)+'.txt'
+        open(opj(lpath,fn),'w').write(rndchr(6)+'.txt')
+
+    # create a bunch of random hidden directories
+    for _ in range(1000) :
+        rnd_dir = '.'+rndchr(6)
+        os.mkdir(opj(lpath,rnd_dir))
+    ##################################################
+
