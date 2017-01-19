191ca58154e2c50a0ec4c98c5ced11ac2e2cff6c
Adam Labadorf <labadorf@bu.edu>
2017-01-18 23:34:50 -0500

    finished first draft lab

--- TerminalQuest.md
+++ TerminalQuest.md
@@ -29,7 +29,7 @@ structure. The levels are as follows:
 
   1. there are a lot of files in this directory, most with random names
   2. there are also a lot of hidden directories with random names
-  3. by doing an ls *SECRET files get printed out that reveal the name of the
+  3. by doing an `ls *SECRET` files get printed out that reveal the name of the
      correct hidden directory to follow
 
 ## Level 4 - looking at files to find the file path to the right answer
@@ -39,5 +39,39 @@ structure. The levels are as follows:
   2. there is a chain of 5 files that lead to the file with the directory name
      of the next level, all the other files contain random filenames that do not
      exist
+  3. the chain starts with init.txt
 
-## Level 5 - 
+## Level 5 - cat files together to print an ascii art message
+
+  1. an ascii art version of the secret directory (.vrkalp) is split one per line
+     across 9 files
+  2. use cat to order the files correctly to read the directory name
+
+## Level 6 - grep lines out of a bunch of a file to find secret messages
+
+  1. `pearls.txt` contains mostly garbage text, but some lines start with SECRET
+  2. grepping out these lines contains interesting science-y quotes, and the last
+     line contains the directory to the next level
+
+## Level 7 - grep lines out of a bunch of files using redirects and cat them together
+
+  1. an ascii art python is split among five different text files that have random
+     text lines, the art lines all start with SECRET
+  2. the art lines are sequential within each file, but are in reverse order wrt
+     the alphabetical sorting of the files
+  3. the directory to level 8 is .python
+
+## Level 8 - head and tail
+
+  1. the top and bottom half of a Dr. Suess ascii art and quote is split between
+     two files, fox.txt and sox.txt, respectively
+  2. taking the appropriate number of head lines of fox.txt and concatenating
+     with the appropriate number of tail lines of sox.txt reveals the full picture,
+     quote, and a column of characters that points you to the last level
+
+## Level 9 - putting it all together
+
+  1. an ascii art of Calvin from Calvin and Hobbes jumping with sunglasses on is
+     spread across multiple files, some with lines starting with SECRET, and others
+     as the head or tail
+  2. students should copy and paste this completed picture into blackboard
--- terminal_quest.py
+++ terminal_quest.py
@@ -4,11 +4,14 @@ import random
 import os
 import shutil
 import string
+import textwrap
 
 # clean
 try :
   shutil.rmtree('level0')
 except Exception,e :
+  print e
+except Error,e :
   print e
 
 def copy_md(level,path) :
@@ -32,7 +35,11 @@ def create_mds(level,path) :
 opj = os.path.join
 
 def rndchr(n) :
-    return ''.join(random.sample(string.ascii_letters,n))
+    if n > len(string.ascii_letters) :
+        rnd = [random.sample(string.ascii_letters,1)[0] for _ in range(n)]
+    else :
+        rnd = random.sample(string.ascii_letters,n)
+    return ''.join(rnd)
 
 if __name__ == '__main__' :
 
@@ -79,7 +86,7 @@ if __name__ == '__main__' :
 
     # create a bunch of random dot directories
     for _ in range(100) :
-      rnd_dir = '.'+rndchr(5)
+      rnd_dir = '.'+rndchr(6)
       os.mkdir(opj(lpath,rnd_dir))
       open(opj(lpath,rnd_dir,'level4.md'),'w').write(
         "# other directories might have what you seek, but not this one"
@@ -109,19 +116,315 @@ if __name__ == '__main__' :
     true_file_path = [rndchr(6)+'.txt' for _ in range(5)]
 
     # create the file chain
-    open(opj(lpath,'start.txt'),'w').write(true_file_path[0])
-    for src,tgt in zip(true_file_path[:-2],true_file_path[1:-1]) :
-        open(opj(lpath,src),'w').write(tgt)
-    open(opj(lpath,true_file_path[-1]),'w').write(level5_dir)
+    open(opj(lpath,'init.txt'),'w').write(true_file_path[0]+'\n')
+    for src,tgt in zip(true_file_path[:-1],true_file_path[1:]) :
+        open(opj(lpath,src),'w').write(tgt+'\n')
+    open(opj(lpath,true_file_path[-1]),'w').write(level5_dir+'\n')
 
     # create a bunch of other files that contain random filenames
-    for _ in range(25) :
+    # or hidden directory names
+    for _ in range(24) :
         fn = rndchr(6)+'.txt'
-        open(opj(lpath,fn),'w').write(rndchr(6)+'.txt')
+        if random.random() > 0.5 :
+            txt = rndchr(6)+'.txt'
+        else :
+            txt = '.'+rndchr(6)
+        open(opj(lpath,fn),'w').write(txt+'\n')
 
     # create a bunch of random hidden directories
-    for _ in range(1000) :
+    for _ in range(999) :
         rnd_dir = '.'+rndchr(6)
         os.mkdir(opj(lpath,rnd_dir))
+        open(opj(lpath,rnd_dir,'level5.md'),'w').write(
+          "# guessing? I *guess* no one's coming?"
+        )
+        open(opj(lpath,rnd_dir,'.level5_hint.md'),'w').write(
+          "# guessing? I *guess* no one's coming?"
+        )
+
     ##################################################
 
+    ##################################################
+    # level 5
+    lpath = opj(lpath,level5_dir)
+    os.mkdir(lpath)
+    create_mds('level5',lpath)
+
+    # write out each line to a different file
+    level6_dir = '.vrkalp'
+    code = [
+         r"                     _              _         "
+        ,r"                    | |            | |        "
+        ,r"     __   __  _ __  | | __   __ _  | |  _ __  "
+        ,r"     \ \ / / | '__| | |/ /  / _` | | | | '_ \ "
+        ,r"  _   \ V /  | |    |   <  | (_| | | | | |_) |"
+        ,r" (_)   \_/   |_|    |_|\_\  \__,_| |_| | .__/ "
+        ,r"                                       | |    "
+        ,r"                                       |_|    "
+    ]
+    code = random.sample(code,len(code))
+    for fn,line in zip(string.ascii_uppercase,code) :
+        open(opj(lpath,fn+'.txt'),'w').write(line+'\n')
+
+    # create a bunch of random hidden directories
+    for _ in range(999) :
+        rnd_dir = '.'+rndchr(6)
+        os.mkdir(opj(lpath,rnd_dir))
+        open(opj(lpath,rnd_dir,'level6.md'),'w').write(
+          "# Informed decision-making comes from a long tradition of guessing and then blaming others for inadequate results. - Scott Adams"
+        )
+        open(opj(lpath,rnd_dir,'.level6_hint.md'),'w').write(
+          "# Informed decision-making comes from a long tradition of guessing and then blaming others for inadequate results. - Scott Adams"
+        )
+
+    ##################################################
+
+    ##################################################
+    # level 6
+    lpath = opj(lpath,level6_dir)
+    os.mkdir(lpath)
+    create_mds('level6',lpath)
+
+    level7_dir = '.'+rndchr(6)
+    quotes = [
+        ("I think it's much more interesting to live not knowing than to have answers which might be wrong.","Richard Feynman")
+       ,("Look deep into nature, and then you will understand everything better.","Albert Einstein")
+       ,("We are drowning in information, while starving for wisdom. The world henceforth will be run by synthesizers, people able to put together the right information at the right time, think critically about it, and make important choices wisely.","E. O. Wilson")
+       ,("The world is full of magical things patiently waiting for our wits to grow sharper.","Bertrand Russell")
+       ,("For me, it is far better to grasp the Universe as it really is than to persist in delusion, however satisfying and reassuring.","Carl Sagan")
+       ,("Science has a simple faith, which transcends utility. It is the faith that it is the privilege of man to learn to understand, and that this is his mission.","Vannevar Bush")
+    ]
+    
+    lines = []
+    for quote,author in quotes :
+        lines.extend([rndchr(60) for _ in range(random.randint(1000,1500))])
+        lines.append('SECRET')
+        for tw in textwrap.wrap(quote,40) :
+            lines.append('SECRET    '+tw)
+        lines.append('SECRET     - '+author)
+    lines.extend([rndchr(60) for _ in range(random.randint(1000,1500))])
+    lines.append('SECRET    '+level7_dir)
+    lines.extend([rndchr(60) for _ in range(random.randint(1000,1500))])
+    open(opj(lpath,'pearls.txt'),'w').write(''.join([_+'\n' for _ in lines]))
+
+    # create a bunch of random hidden directories
+    for _ in range(999) :
+        rnd_dir = '.'+rndchr(6)
+        os.mkdir(opj(lpath,rnd_dir))
+        open(opj(lpath,rnd_dir,'level6.md'),'w').write(
+          "# by now, you should know this strategy doesn't work"
+        )
+        open(opj(lpath,rnd_dir,'.level6_hint.md'),'w').write(
+          "# by now, you should know this strategy doesn't work"
+        )
+    ##################################################
+
+    ##################################################
+    # level 7
+    lpath = opj(lpath,level7_dir)
+    os.mkdir(lpath)
+    create_mds('level7',lpath)
+
+    snake = r"""
+                    /^\/^\
+                  _|__| O |
+         \/     /~     \_/ \
+          \____|__________/  \
+                 \_______      \
+                         `\     \                 \
+                           |     |                  \
+                          /      /                    \
+                         /     /                       \\
+                       /      /                         \ \
+                      /     /                            \  \
+                    /     /             _----_            \   \
+                   /     /           _-~      ~-_         |   |
+                  (      (        _-~    _--_    ~-_     _/   |
+                   \      ~-____-~    _-~    ~-_    ~-_-~    /
+                     ~-_           _-~          ~-_       _-~   - jurcy -
+                        ~--______-~                ~-___-~""".split('\n')
+
+    linesperbit = 4
+    snakebits = [snake[_:_+linesperbit] for _ in range(0,len(snake),linesperbit)]
+    for fn,snakebit in zip(string.ascii_uppercase,snakebits[::-1]) :
+        lines = []
+        lines.extend([rndchr(60) for _ in range(random.randint(1000,1500))])
+        lines.extend(['SECRET'+_ for _ in snakebit])
+        lines.extend([rndchr(60) for _ in range(random.randint(300,1000))])
+        open(opj(lpath,fn+'.txt'),'w').write(''.join(_+'\n' for _ in lines))
+
+    for _ in range(999) :
+        rnd_dir = '.'+rndchr(6)
+        os.mkdir(opj(lpath,rnd_dir))
+        open(opj(lpath,rnd_dir,'level7.md'),'w').write(
+          "# I've got a lovely bunch of coconuts, diddle dee dee"
+        )
+        open(opj(lpath,rnd_dir,'.level7_hint.md'),'w').write(
+          "# I've got a lovely bunch of coconuts, diddle dee dee"
+        )
+    level8_dir = '.python'
+    ##################################################
+
+    ##################################################
+    # level 8
+    lpath = opj(lpath,level8_dir)
+    os.mkdir(lpath)
+    create_mds('level8',lpath)
+
+    suess = r"""
+
+        .;''-.
+      .' |    `._
+     /`  ;       `'.
+   .'     \         \
+  ,'\|    `|         |
+  | -'_     \ `'.__,J
+ ;'   `.     `'.__.'
+ |      `"-.___ ,'
+ '-,           /
+ |.-`-.______-|
+ }      __.--'L
+ ;   _,-  _.-"`\         ___
+ `7-;"   '  _,,--._  ,-'`__ `.
+  |/      ,'-     .7'.-"--.7 |        _.-'
+  ;     ,'      .' .'  .-. \/       .'
+   ;   /       / .'.-     ` |__   .'        I like
+    \ |      .' /  |    \_)-   `'/   _.-'`` nonsense . It
+     _,.--../ .'     \_) '`_      \'`   wakes up the b rain cells.
+   '`f-'``'.`\;;'    ''`  '-`      |    Fantasy is a n ecessary ingredient
+      \`.__. ;;;,   )              / in living; it's a way of looking
+       `-._,|;;;,, /\            ,'  at life through t he wrong end of a
+        / /<_;;;;'   `-._    _,-'                    t elescope.
+       | '- /;;;;;,      `t'` \.     - Theodor Seuss G eisel
+       `'-'`_.|,';;;,      '._/|     
+       ,_.-'  \ |;;;;;    `-._/
+             / `;\ |;;;,  `"     
+           .'     `'`\;;, /
+          '           ;;;'|
+              .--.    ;.:`\    _.--,
+             |    `'./;' _ '_.'     |
+              \_     `"7f `)       /
+              |`   _.-'`t-'`"-.,__.'
+              `'-'`/;;  | |   \ mx
+                  ;;;  ,' |    `
+                      /   ' """.split('\n')
+    top_suess = suess[:int(len(suess))/2+3]
+    lines = [rndchr(60) for _ in range(random.randint(1000,1500))]
+    open(opj(lpath,'fox.txt'),'w').write(''.join(_+'\n' for _ in top_suess+lines))
+
+    tail_suess = suess[len(top_suess):]
+    lines = [rndchr(60) for _ in range(random.randint(1000,1500))]
+    open(opj(lpath,'sox.txt'),'w').write(''.join(_+'\n' for _ in lines+tail_suess))
+
+    for _ in range(999) :
+        rnd_dir = '.'+rndchr(6)
+        os.mkdir(opj(lpath,rnd_dir))
+        open(opj(lpath,rnd_dir,'level7.md'),'w').write(
+          "# I do not like them, Sam I am, I do not like bad guesses and spam"
+        )
+        open(opj(lpath,rnd_dir,'.level7_hint.md'),'w').write(
+          "# I do not like them, Sam I am, I do not like bad guesses and spam"
+        )
+
+    level9_dir = '.bnattG'
+    ##################################################
+
+    ##################################################
+    # level 9
+    lpath = opj(lpath,level9_dir)
+    print lpath
+    os.mkdir(lpath)
+    create_mds('level9',lpath)
+
+    calvin = """
+                                    oooo   ooo
+                                   $   $  $   $
+                                   \"o  $ $  o\"\"
+                                     o  \"   \"ooooo
+                                 oo \"\"           o$
+                   o            o            oo  \"
+                  $$             $o$\"\"$o  ooo$
+                  $\"$          o $    \"$  $
+        o$o       $ \"$         $ $     $ $
+         $$$o     $$ \"$       o$ $     o $o
+         \"$ \"\"o   \"$   \"o     $$ \"o     o\"
+     $o   $$   \"o  $     \"oo  $\"  $   o$\"
+      \"$   $o    \"o$$       \"o$    $o$\" oo$
+        \"o \"$o                     \"$o $\"$$
+          \"        oo$$$$$$$oo        $oo$$\"\"      o\" o
+ \"\"\"\"\"\"\"\"\"      o$$$$$$$$$$$\"$o             o\"\"\"$o$$  o$
+       ooo$$$\"o$$$$$$$$$$$$$$ \"$o    o   o$$$o   $ $ o$
+    o$$$$$$$$$$$$$$$$$$$$$$$$    \"oo  o      \"\"o  \"$ $
+   $$$$$$$$$$$$$$$$$$$$$$$$$$      \"$o   o$$\"\"\"$     \" oo\"\"o
+o\"\"\"\"$$$$$$$$$$$$$$$$$$$$$$\"         \"\"$o\"$o          \"   o$
+     \"$$$$$$$$\"\"\"\"$\"\"$$$\"\"              \"$oo$\"\"$o     o$\"\"\"
+      $$$$$$$\"                           $\"\"\"\"$\"  o\"\"\"\"
+       $\"\"\"\"\"$ooooo        ooooo$$$$$$$     o$\" o\"
+        $     \"\"\"\" oooo$$$$$$$$$$$$$$\"     $\"  o\"
+      oo$   oooo$$$$$$$$$$$\"\"\"\"\"$$$$\"    o$\" o$\"
+    \"$ $o$$$$$$$$$$$$$\"\"$     o$$$\"oooo$\"  o\"
+      \"o$ \"$$$$$$$$$$$$         $$o$\"$$$   $\"
+        \"$  \"\"$$$$$$$$$        o$\"$$$ \"$$o$$
+          \"o   \"\"$$$$$$o     o$$$$ \"\"$o \"\"\"$
+            \"$o    \"\"$$$$$$o\"  o$$$$oo o$$$$
+               \"\"$oo     $$\" \"$$$\"\" ooooooo$
+                    \"\"\"\"$\"  o$\"   oo$$$$$\"\"$$
+                       $ oo$\"  o$$$$$\"\"  ooo$
+                       $o$\"  o$$$$\"  oo$$$$$$$o
+                        $$ o$$$\"  o$$$$$$\"\"\"\"\"$o
+                         \"o$$\"  o$$$$\"\"  o$$$$$$$o
+                           \"$oo$$$$\"  o$$$$$\"\"\" o$o
+                             \"$$$\" oo$$$\"\" oo$$$$$$$
+                          ooooo$oo$$$\"\" oo$$$$\"\"\"$$\"\"
+                         $\"oooo $$$\" o$$$$\"\"      $
+                       o$\"o$   $$\"oo$$\"\"       \" o$
+                       $ o$$o  $$o$$\"          oo$$
+                       $ $$$$  $$$$$$$$$$$$$$$$$$$$
+                       $ $$$$  $$$$$$$$$$$$$$$$$$$$\"
+                       $ $$$$  $$$$$$$$$$$$$$$$$$$$
+                       $ \"\"    \"\"$$$$$$$$$\"\"\"$\"\"\"\"
+                       $o         $\"$\"    \" $\"
+                        $o       $$  $o    o$
+                         \"$o   o$$    \"\"$$$\"
+                           \"\"\"\"\"\" """.split('\n')
+    linesperbit = 5
+    calvinbits = [calvin[_:_+linesperbit] for _ in range(0,len(calvin),linesperbit)]
+
+    secret_ids = random.sample(range(len(calvinbits)),5)
+    head_tail_ids = set(range(len(calvinbits))).difference(set(secret_ids))
+    head_ids = random.sample(head_tail_ids,3)
+    tail_ids = set(head_tail_ids).difference(set(head_ids))
+
+    print secret_ids
+    print head_ids
+    print tail_ids
+
+    # do the secret ones
+    for cid in secret_ids :
+        calvinbit = calvinbits[cid]
+        lines = []
+        lines.extend([rndchr(60) for _ in range(random.randint(300,1000))])
+        lines.extend(['SECRET    '+_ for _ in calvinbit])
+        lines.extend([rndchr(60) for _ in range(random.randint(300,1000))])
+        fn = rndchr(6)+'.txt'
+        open(opj(lpath,fn),'w').write(''.join(_+'\n' for _ in lines))
+
+    # do the heads
+    for cid in head_ids :
+        calvinbit = calvinbits[cid]
+        lines = []
+        lines.extend(['          '+_ for _ in calvinbit])
+        lines.extend([rndchr(60) for _ in range(random.randint(300,1000))])
+        fn = rndchr(6)+'.txt'
+        open(opj(lpath,fn),'w').write(''.join(_+'\n' for _ in lines))
+
+    # do the tails
+    for cid in tail_ids :
+        calvinbit = calvinbits[cid]
+        lines = []
+        lines.extend([rndchr(60) for _ in range(random.randint(300,1000))])
+        lines.extend(['          '+_ for _ in calvinbit])
+        fn = rndchr(6)+'.txt'
+        open(opj(lpath,fn),'w').write(''.join(_+'\n' for _ in lines))
+
+    ##################################################
