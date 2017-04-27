e4875e386156b53a649e5e562c5a011873301399
Adam Labadorf <alabadorf@gmail.com>
2017-04-27 15:20:53 -0400

    updated setup.py, VERSION, and LICENSE

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@
 
 from setuptools import setup, find_packages
 
-setup(name='terminal_quest',
-      version='1.0'
+setup(name='terminal_quest'
+      ,version=open('VERSION').read().strip()
       ,description='Cute little puzzle for teaching basic command line usage'
       ,author='Adam Labadorf'
       ,author_email='labadorf@bu.edu'
       ,packages=find_packages()
       ,package_data={'terminal_quest': ['mds/*','mds/.level*']}
+      ,install_requires=[
+        'future'
+        ,'fabulous'
+      ]
       ,entry_points={
         'console_scripts': [
           'terminal_quest=terminal_quest:main'
