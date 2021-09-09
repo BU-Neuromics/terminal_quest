586fb80d681d89851869c8715b7eb8fc84900b77
Adam Labadorf <labadorf@bu.edu>
2021-09-08 23:30:58 -0400

    added pypi build and upload script

---
+++ build_pypi.sh
@@ -0,0 +1,11 @@
+set -eu
+
+rm -rf dist/*
+python setup.py sdist bdist_wheel
+
+# twine_creds.sh mush define TWINE_USERNAME and TWINE_PASSWORD env vars
+. twine_creds.sh
+
+echo $TWINE_USERNAME
+
+twine upload -u $TWINE_USERNAME -p $TWINE_PASSWORD dist/*
